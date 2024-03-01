e0086d63e7095aaa5f6b1874d06bf097f681b06d
KevinTo <khoatonguyen@gmail.com>
2024-03-01 16:42:20 -0500

    Added MainView and DashboardView

--- E-Commerce-Swift/E-Commerce-Swift/Views/DashboardView.swift
+++ E-Commerce-Swift/E-Commerce-Swift/Views/DashboardView.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct DashboardView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+            VStack {
+                Image("user")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 170)
+                    .padding(.vertical, 32)
+                VStack {
+                    HStack(spacing: 30) {
+                        Text("Username")
+                        
+                        Text("Phone Number")
+                    }
+                    .padding()
+                    Text("Email Address")
+                    
+                    Spacer()
+                    
+                    Text("App Version 1.0.0")
+                        .padding(.bottom, 8)
+                }
+                
+            }
+            .navigationBarTitle("Dashboard", displayMode: .inline)
+        }
     }
 }
 
--- E-Commerce-Swift/E-Commerce-Swift/Views/MainView.swift
+++ E-Commerce-Swift/E-Commerce-Swift/Views/MainView.swift
@@ -9,14 +9,46 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image("logo")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100, height: 120)
-                .padding(.vertical, 32)
+        NavigationStack {
+            VStack {
+                
+                Image(systemName: "photo.on.rectangle.angled") // Placeholder for item picture
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .padding()
+                
+                VStack(spacing: 20) { // Add spacing between buttons
+                    Button(action: {
+                        // Action for take picture
+                    }, label: {
+                        Text("TAKE PICTURE")
+                            .font(.title2)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .frame(height: 50) // Set the height for the button
+                    
+                    Button(action: {
+                        // Action for upload picture
+                    }, label: {
+                        Text("UPLOAD PICTURE")
+                            .font(.title2)
+                    })
+                    .buttonStyle(.borderedProminent)
+                    .frame(height: 50) // Set the height for the button
+                }
+                .padding(.horizontal) // Add horizontal padding to the VStack containing buttons
+                
+                
+            }
+            .padding(.bottom, 32) // Add padding at the bottom of the VStack
+            .navigationBarTitle("Home", displayMode: .inline)
+            .navigationBarItems(trailing:
+                                    NavigationLink(destination: DashboardView()) {
+                Text("Dashboard")
+            }
+            )
         }
-        .padding()
     }
 }
 
